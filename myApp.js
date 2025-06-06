85822af507ef95250fe37104f568cfca5c378a8b
saba-1A <sabafathima789nazar@gmail.com>
2025-06-06 13:07:08 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,18 +1,20 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
 
-// Serve static assets
 app.use('/public', express.static(__dirname + '/public'));
 
-// Serve the HTML file at "/"
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// Serve JSON at "/json"
 app.get('/json', function(req, res) {
-  res.json({ message: "Hello json" });
+  let message = "Hello json";
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    message = message.toUpperCase();
+  }
+  res.json({ message: message });
 });
 
 module.exports = app;
