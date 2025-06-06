439b7009b1b455f58af2829f45f2dfe5abbae9bc
saba-1A <sabafathima789nazar@gmail.com>
2025-06-06 13:31:45 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,20 +1,18 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const path = require('path');
-
-// Root-level logger middleware
+// Logger middleware (from before)
 app.use(function(req, res, next) {
   console.log(`${req.method} ${req.path} - ${req.ip}`);
   next();
 });
 
-app.use('/public', express.static(__dirname + '/public'));
-
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+// /now route with chained middleware
+app.get('/now', (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+}, (req, res) => {
+  res.json({ time: req.time });
 });
 
+// Other routes like /json and /
 app.get('/json', function(req, res) {
   let message = "Hello json";
   if (process.env.MESSAGE_STYLE === "uppercase") {
@@ -23,4 +21,6 @@ app.get('/json', function(req, res) {
   res.json({ message: message });
 });
 
-module.exports = app;
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
