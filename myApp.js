76a2538788dd55e5c032005ecd4382c93f6ca2d2
saba-1A <sabafathima789nazar@gmail.com>
2025-06-06 13:02:46 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -2,12 +2,17 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// Serve static assets from the /public folder
+// Serve static assets
 app.use('/public', express.static(__dirname + '/public'));
 
-// Serve the HTML file on root route
+// Serve the HTML file at "/"
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
+});
+
+// Serve JSON at "/json"
+app.get('/json', function(req, res) {
+  res.json({ message: "Hello json" });
 });
 
 module.exports = app;
