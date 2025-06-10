9115871dfcee48d361641348353a5bce441ef23a
saba-1A <sabafathima789nazar@gmail.com>
2025-06-10 18:49:46 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -8,34 +8,42 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // 2. Logger middleware (root-level)
 app.use((req, res, next) => {
-  console.log(`${req.method} ${req.path} - ${req.ip}`);
-  next();
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
 });
 
 // 3. /now route with chained middleware (current time)
 app.get('/now',
-  (req, res, next) => {
-    req.time = new Date().toString();
-    next();
-  },
-  (req, res) => {
-    res.json({ time: req.time });
-  }
+    (req, res, next) => {
+        req.time = new Date().toString();
+        next();
+    },
+    (req, res) => {
+        res.json({ time: req.time });
+    }
 );
 
 // 4. /json route influenced by MESSAGE_STYLE env var
 app.get('/json', (req, res) => {
-  let message = "Hello json";
-  if (process.env.MESSAGE_STYLE === "uppercase") {
-    message = message.toUpperCase();
-  }
-  res.json({ message });
+    let message = "Hello json";
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        message = message.toUpperCase();
+    }
+    res.json({ message });
 });
 
 // 5. Root route serving the HTML page
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-// Export the app
+// 6. Start the server - THIS IS CRITICAL FOR RENDER
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    console.log(`Open in browser: http://localhost:${PORT}`); // For local testing
+});
+
+
+// Export the app 
 module.exports = app;
