c9d2d422f8eed4f212747c12115a9f3fc6f31964
saba-1A <sabafathima789nazar@gmail.com>
2025-06-16 21:53:51 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -4,12 +4,10 @@ var app = express();
 /** 8) Chaining middleware. A Time server */
 
 app.get('/now', function(req, res, next) {
-  req.time = new Date().toString(); // ✅ Add current time to request object
-  next(); // ✅ Pass control to next function
+  req.time = new Date().toString();
+  next();
 }, function(req, res) {
-  res.json({ time: req.time }); // ✅ Respond with time in JSON format
+  res.json({ time: req.time });
 });
 
-//---------- DO NOT EDIT BELOW THIS LINE --------------------
 module.exports = app;
-s
