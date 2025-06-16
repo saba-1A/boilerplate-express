f1321ff3c33c9c4e92c30e603d22d1eabd049faa
saba-1A <sabafathima789nazar@gmail.com>
2025-06-16 21:46:10 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,58 +1,19 @@
-
 var express = require('express');
 var app = express();
+const bodyParser = require('body-parser'); // ✅
 
-// --> 7)  Mount the Logger middleware here
+app.use((req, res, next) => {
+ console.log(req.method + " " + req.path + " - " + req.ip); 
+ next();
+});
 
+app.use(bodyParser.urlencoded({ extended: false })); // ✅
 
-// --> 11)  Mount the body-parser middleware  here
+...
 
-
-/** 1) Meet the node console. */
-
-console.log("Hello World");
-
-/** 2) A first working Express Server */
-
-
-/** 3) Serve an HTML file */
-
-
-/** 4) Serve static assets  */
-
-
-/** 5) serve JSON on a specific route */
-
-
-/** 6) Use the .env file to configure the app */
- 
- 
-/** 7) Root-level Middleware - A logger */
-//  place it before all the routes !
-
-
-/** 8) Chaining middleware. A Time server */
-
-
-/** 9)  Get input from client - Route parameters */
-
-
-/** 10) Get input from client - Query parameters */
-// /name?first=<firstname>&last=<lastname>
-
-  
-/** 11) Get ready for POST Requests - the `body-parser` */
-// place it before all the routes !
-
-
-/** 12) Get data form POST  */
-
-
-
-// This would be part of the basic setup of an Express app
-// but to allow FCC to run tests, the server is already active
-/** app.listen(process.env.PORT || 3000 ); */
-
-//---------- DO NOT EDIT BELOW THIS LINE --------------------
-
- module.exports = app;
+app.get('/now', function(req, res, next){
+  req.time = new Date().toString(); // ✅ Add time here
+  next();  
+}, function(req, res) {
+  res.json({ time: req.time }); // ✅ Return JSON
+});
